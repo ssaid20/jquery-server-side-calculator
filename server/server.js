072e2be6af55d5f937dc0bc343216394bf658aca
Said Said <ssaid20@icloud.com>
2023-09-10 18:50:43 -0500

    setup the router

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+const express = require('express'); // Require the express module.
+const app = express(); // Create an express application.
+const port = 3000; // Define the port on which the server will be running.
 
+let history = []; // Create an array to store the history of calculations.
+
+app.use(express.static('server/public')); // Serve static files from the 'server/public' directory.
+app.use(express.urlencoded({ extended: true })); // Use the urlencoded parser middleware.
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); 
+});
